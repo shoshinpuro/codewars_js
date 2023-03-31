ede5f5b7f9ffec095b9d3cca81c74d47d49842ce
Alex Maryasov <shtilnuhcenberg@gmail.com>
2023-03-31 20:13:13 +0300

    optimize code in build-tower

--- Build-Tower.js
+++ Build-Tower.js
@@ -1,21 +1,13 @@
 function towerBuilder(nFloors) {
-    let arr= [];
-    let length = nFloors*2-1
-    for(let i=0; i<nFloors; i++){
-      let str = '';
-      let count = i+1;
-      while(count>0){
-        str += '*'
-        if(str!='*'){
-          str += '*';
-        }
-        count--;
-      }
-      while(str.length < length){
-        str += ' ';
-        str = ' ' + str;
-      }
-      arr.push(str);
+    let bricks = '*', 
+        spaces = ' '.repeat(nFloors - 1), 
+        tower = [];
+  
+    for(let i = 0; i < nFloors; i++) { 
+      let space = spaces.substr(0, nFloors - i - 1); 
+      tower.push(space + bricks + space); 
+      bricks+='**'; 
     }
-    return arr;
+  
+    return tower;
   }
